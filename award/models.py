212d89bcd7ddef383f2abf2897eecd196b4ae476
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-24 13:07:59 +0300

    Added models to create a database schema

--- award/models.py
+++ award/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Projects(models.Model):
+    name=models.CharField(max_length=30)
+    image=models.ImageField(upload_to='projects/')
+    design=models.IntegerField()
+    usability=models.IntegerField()
+    content=models.IntegerField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    description=models.TextField(max_length=320)
+    link=models.CharField(max_length=30)
+
+
+class Profile(models.Model):
+    profile=models.ImageField(upload_to='profile/')
+    bio=models.CharField(max_length=30)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    phone=models.IntegerField()
