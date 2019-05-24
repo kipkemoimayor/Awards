736d72950dc2f9586579dd836dbaa93a43a3316e
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-24 18:37:45 +0300

    Created a post request to Database

--- award/models.py
+++ award/models.py
@@ -5,16 +5,16 @@ from django.contrib.auth.models import User
 class Projects(models.Model):
     name=models.CharField(max_length=30)
     image=models.ImageField(upload_to='projects/')
-    design=models.IntegerField()
-    usability=models.IntegerField()
-    content=models.IntegerField()
+    design=models.IntegerField(default=0)
+    usability=models.IntegerField(default=0)
+    content=models.IntegerField(default=0)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     description=models.TextField(max_length=320)
-    link=models.CharField(max_length=30)
+    link=models.CharField(max_length=60)
 
 
 class Profile(models.Model):
     profile=models.ImageField(upload_to='profile/')
-    bio=models.CharField(max_length=30)
+    bio=models.CharField(max_length=60)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     phone=models.IntegerField()
--- award/views.py
+++ award/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 
@@ -8,7 +8,16 @@ def index(request):
 
 @login_required(login_url='/accounts/login/')
 def post(request):
-    form=PostForm()
+    current_user=request.user
+    if request.method=='POST':
+        form =PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.user=current_user
+            post.save()
+        return redirect("index")
+    else:
+        form=PostForm()
     return render(request,'post.html',{'form':form})
 @login_required(login_url='/accounts/login/')
 def profile(request):
