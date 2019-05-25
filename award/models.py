7ae25aabe6b59a931ec361d80a656882ccc801fc
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-25 12:24:12 +0300

    Added Migrations

--- award/models.py
+++ award/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 # Create your models here.
 
 class Projects(models.Model):
@@ -21,8 +23,8 @@ class Profile(models.Model):
     phone=models.IntegerField()
 
 class Rates(models.Model):
-    design=models.IntegerField(default=0)
-    usability=models.IntegerField(default=0)
-    content=models.IntegerField(default=0)
+    design=models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    usability=models.PositiveIntegerField(default=0)
+    content=models.PositiveIntegerField(default=0)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
-    project=models.ForeignKey(Projects,on_delete=models.CASCADE)
+    project=models.IntegerField(default=0)
